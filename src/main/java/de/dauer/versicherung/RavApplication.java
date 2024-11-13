b6d7a1ab87da011decc694197a3545e12aff3c1f
Liubov Dauer <lyubov.dauer@gmail.com>
2024-11-13 21:27:37 +0100

    Anwendung ist inizialisiert

---
+++ src/main/java/de/dauer/versicherung/RavApplication.java
@@ -0,0 +1,13 @@
+package de.dauer.versicherung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/dauer/versicherung/RavApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dauer.versicherung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
